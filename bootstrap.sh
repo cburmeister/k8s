cf42074ce70ec27574b14e4ff94b8e6af8b9c2a4
Corey Burmeister <burmeister.corey@gmail.com>
2021-02-11 10:25:18 -0800

    Shift cert-manager to kustomize/apps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
 kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.14.1/controller.yaml
