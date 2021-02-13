935c45a193a4af4fab9923856d6faaddaeb8a7bd
Corey Burmeister <burmeister.corey@gmail.com>
2021-02-12 17:57:54 -0800

    Shift ingress-nginx to kustomize/apps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/do/deploy.yaml
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
 kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.14.1/controller.yaml
